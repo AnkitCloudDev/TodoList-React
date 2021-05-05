c0fd1608407032fa486b3639fb2da42ef6bfae2e
unknown <ankitdongre96@gmail.com>
2021-05-05 19:33:44 +0100

    Added handlers for username,password and login

--- src/components/todo/Login/Login.js
+++ src/components/todo/Login/Login.js
@@ -5,19 +5,46 @@ class Login extends Component{
     state = {
         username: '',
         password: '',
+        isAuthenticated: false,
+        loginFailed: false,
     };
 
-    usernameHandler = () => {
-
+    usernameHandler = (event) => {
+        // console.log(event.target.value);
+        this.setState({[event.target.name]: event.target.value})
     }
+
+    loginHandler = () =>{
+        if(this.state.username === 'ankit' && this.state.password === 'password')
+        {
+            console.log("Logged In");
+            this.setState({isAuthenticated: true});
+            this.setState({loginFailed: false});
+        } 
+        else {
+            this.setState({loginFailed: true});
+        }
+    }
+  
     render(){
+        let text = null;
+        if(this.state.isAuthenticated)
+        {
+            text = <p>Login Successful!</p>
+        }
+
+        if(this.state.loginFailed)
+        {
+            text = <p>Invalid Credentials!!</p>
+        }
+        
         return(<div>
-            <div className="LoginPage">
-            <form className="Login">
-                User Name: <input type="text" name="username" placeholder="Enter user name" onChange={this.usernameHandler}/>
-                Password: <input type="password" name="password"placeholder="Enter password"/>
-                <button type="submit" >Login</button>
-            </form>
+            {text}
+            <div className="LoginPage" method="post">           
+                User Name: <br></br><input type="text" name="username" placeholder="Enter user name" onChange={this.usernameHandler}/>
+                <br></br>Password: <br></br><input type="password" name="password"placeholder="Enter password" onChange={this.usernameHandler}/>
+                <br></br><button onClick={this.loginHandler}>Login</button>
+           
             </div> 
 
         </div>);
