da341c0aad71a3b8c66b1f70f152b701d9d39d8d
unknown <ankitdongre96@gmail.com>
2021-05-05 20:53:08 +0100

    Added React Router dom dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
-import Todo from './components/todo/TodoApp';
+import TodoApp from './components/todo/TodoApp';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Todo></Todo>
+      <BrowserRouter>
+      <TodoApp></TodoApp>
+      </BrowserRouter>
+    
+     
+    
+     
     </div>
   );
 }
--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import LoginComponent from './Login/Login';
+import Login from './Login/Login';
 import './TodoApp.css';
+import { Router,Route, Switch} from 'react-router-dom';
+
 class TodoApp extends Component{
     render()
     {
         return( 
         <div >
-            <LoginComponent/>
+            {/* <LoginComponent/> */}
+
+     <Router>
+                <>
+                <Route path="/login" component={Login} > </Route>
+                </>
+
+     </Router>
+ 
+      {/* <Route path="/welcome" exact component=""> </Route> */}
+      {/* </Switch> */}
                   
         </div>);
     }
