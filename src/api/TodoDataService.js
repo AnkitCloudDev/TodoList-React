373c1de5644fe0eadd93f71677602bb0faa55be6
unknown <ankitdongre96@gmail.com>
2021-05-24 20:54:46 +0100

    Added DELETE buttong and function to delete in TodoDataService"

--- src/api/TodoDataService.js
+++ src/api/TodoDataService.js
@@ -7,6 +7,13 @@ class TodoDataService {
         return axios.get(baseUrl);
     }
 
+    deleteTodo(name,id)
+    {
+        return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`);
+    }
+
+
+
 }
 
 export default new TodoDataService();
--- src/components/todo/TodoList/TodoList.js
+++ src/components/todo/TodoList/TodoList.js
@@ -24,35 +24,70 @@ class TodoList extends Component{
         //     targetDate: new Date()
         // },
     
-    ]
+    ],
+    message: null
        
     }
     componentDidMount(){
+            console.log("CDM CALL");
+            this.refreshTodos();
+    }
+    deleteTodoList = (id)=>{
+        let username = AuthenticationService.getUserName();
+        TodoDataService.deleteTodo(username,id).then(
+                response =>
+          {  this.refreshTodos();
+            //   console.log(response);
+            //   if(response.status === 200)
+            //    {
+            //       this.setState({message: `Deletion of Todo${id} Successful`});
+                  
+            //    }
+            //    else 
+            //     {
+            //          this.setState({message: `Deletion of Todo${id} Failed`})
+                    
+            //     }
+               
+        }
+            
+        ).catch(
+            error => console.log(error)
+        );
+
+    }
+
+    refreshTodos(){
         TodoDataService.fetchTodoData(AuthenticationService.getUserName()).then(response => 
             this.setState({todos: response.data})
         ).catch(error => console.log(error));
     }
 
     render(){
-        
+
         let todoList= this.state.todos.map( i =>{
             console.log(i);
             return <tr key={i.id}>
                 <td>{i.description}</td>
                 <td>{i.targetDate}</td>
                 <td>{i.isDone ? 'Yes': 'No'}</td>
+                <td><button className="btn btn-warning" onClick={()=>this.deleteTodoList(i.id)}>DELETE</button></td>
             </tr>;
         } );
 
+   
+
         return <div>
             <h1>List Todos</h1>
-            <div className="container">         
+            <div className="container">
+               {this.state.message != null ? <div className="alert alert-success">{this.state.message}</div> : null  }      
             <table className="table">
                 <thead>
                 
                    <th>description</th>
                    <th>Target Date</th>
                    <th>Finished?</th>
+                   <th>Delete</th>
                 </thead>
                 <tbody>
                  {todoList}
